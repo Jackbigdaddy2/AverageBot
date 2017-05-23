e0251bc33bb9d4766d0376e0bec1e2ea5b6b3dd8
McNugget <dp101801@gmail.com>
2017-05-23 10:08:09 -0400

    removed googleimage command for now.

--- cogs/testserver.py
+++ cogs/testserver.py
@@ -23,9 +23,7 @@ import subprocess
 from .utils.dataIO import dataIO
 from .utils import checks
 import sys
-import urllib2
 import simplejson
-from urllib import FancyURLopener
 client = discord.Client()
 
 
@@ -136,54 +134,6 @@ class AverageCogs:
         await self.bot.send_message(ctx.message.channel, embed=em)
         
         
-        
-        
-        
-        
-   
-    @commands.command(aliases=['gi'], pass_context=True, no_pm=True)
-    async def googleimage(self, ctx, *, SearchTerm):
-        # Define search term
-        searchTerm = SearchTerm
-
-        # Replace spaces ' ' in search term for '%20' in order to comply with request
-        searchTerm = searchTerm.replace(' ','%20')
-
-
-        # Start FancyURLopener with defined version 
-        class MyOpener(FancyURLopener): 
-            version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
-        myopener = MyOpener()
-
-        # Set count to 0
-        count= 0
-
-        for i in range(0,10):
-            # Notice that the start changes for each iteration in order to request a new set of images for each loop
-            url = ('https://ajax.googleapis.com/ajax/services/search/images?' + 'v=1.0&q='+searchTerm+'&start='+str(i*4)+'&userip=MyIP')
-            request = urllib2.Request(url, None, {'Referer': 'testing'})
-            response = urllib2.urlopen(request)
-
-            # Get results using JSON
-            results = simplejson.load(response)
-            data = results['responseData']
-            dataInfo = data['results']
-
-            # Iterate for each result and get unescaped url
-            for myUrl in dataInfo:
-                count = count + 1
-
-                #myopener.retrieve(myUrl['unescapedUrl'],str(count)+'.jpg')
-                await self.bot.send_file(ctx.message.channel, myUrl['unescapedUrl'],str(count)+'.jpg')
-
-            # Sleep for one second to prevent IP blocking from Google
-            time.sleep(1)
-        
-    
-
-
-
-
 
 def setup(bot):
     if soupAvailable:
