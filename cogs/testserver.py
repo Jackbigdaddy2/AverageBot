eae61c7779a4d30ed512196833d222fe9a77051e
McNugget <dp101801@gmail.com>
2017-05-24 11:41:39 -0400

    Added Info Command

--- cogs/testserver.py
+++ cogs/testserver.py
@@ -144,6 +144,13 @@ class AverageCogs:
         gs = goslate.Goslate()
         await self.bot.say(gs.translate(Text, Language))
         
+        
+    @commands.command(pass_context=True, no_pm=True)
+    async def info(self, ctx)
+        Description = 'Youtube - https://www.youtube.com/channel/UC4SQr97CBGMGPpdRpThOu9Q\nTwitch - https://www.twitch.tv/averagefriends\nDiscord Server - https://discord.me/Avrg'
+        em = discord.Embed(title='Average Friends Info', description=Description, colour=ChosenColor)
+            em.set_author(name=ctx.message.author, icon_url=ctx.message.author.avatar_url)
+            await self.bot.send_message(ctx.message.channel, embed=em)
 
 def setup(bot):
     if soupAvailable:
